2484c1bb0686d7aa0e41ac67541962d6cbaee23f
yonggwonk <gwonluck@dgu.ac.kr>
2023-10-31 18:58:44 +0900

    feat: Fix activateVenv.py
    환경변수 파일을 새로 업로드하여 다른 로컬 환경에서 다운받아 사용받을 수 있도록 함

--- required/activateVenv.py
+++ required/activateVenv.py
@@ -1,35 +1,27 @@
+import gdown
 import os
 import tarfile
-import platform
-import subprocess
 
-def unpack_and_activate_environment(env_archive_path, env_directory):
-    try:
-        # Create the target directory if it doesn't exist
-        os.makedirs(env_directory, exist_ok=True)
+# 파일 ID와 다운로드 경로를 설정
+file_link = "https://drive.google.com/file/d/1i7M8HG-iRNU7IlOp-H9MiD9HggwQZOK8/view?usp=drive_link"
+file_id = file_link.split("/d/")[1].split("/view")[0]
 
-        # Unpack the environment from the archive
-        with tarfile.open(env_archive_path, "r:gz") as tar:
-            tar.extractall(path=env_directory)
-            print(f"Unpacked the environment into {env_directory}")
+# 상위 폴더로 이동
+parent_directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
-        # Activate the environment
-        if platform.system() == 'Windows':
-            activate_script = os.path.join(env_directory, "Scripts", "activate.bat")
-            subprocess.run(activate_script, shell=True)
-        else:
-            activate_script = os.path.join(env_directory, "bin", "activate")
-            subprocess.run(f"source {activate_script}", shell=True)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+# 다운로드한 파일을 저장할 경로
+output_path = os.path.join(parent_directory, 'project_env.tar.gz')
 
-if __name__ == "__main__":
-    env_archive_path = "project_env.tar.gz"  # 배포된 환경 압축 파일 경로
-    env_directory = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), "venv")  # 한 단계 상위 디렉토리에 환경 디렉토리 생성
+# 파일 다운로드
+gdown.download(f'https://drive.google.com/uc?id={file_id}', output_path, quiet=False)
 
-    # Unpack and activate the environment
-    unpack_and_activate_environment(env_archive_path, env_directory)
+# 압축을 해제할 경로
+new_directory_name = 'project_env'  # 새로운 디렉토리 이름
+extraction_directory = os.path.join(parent_directory, new_directory_name)
 
-"""
-가상환경 실행 코드 수정 필요
-"""
+# 압축 해제
+with tarfile.open(output_path, 'r:gz') as tar:
+    tar.extractall(path=extraction_directory)
+
+# 다운로드한 tar.gz 파일 삭제
+os.remove(output_path)
