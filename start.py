62cf27ceec88b54fd732487042e2f97a7455eb92
DaehoGo <rualneox@dgu.ac.kr>
2023-12-14 23:26:30 +0900

    feat: fix all
    전체 경로 수정

--- start.py
+++ start.py
@@ -2,11 +2,23 @@ import subprocess
 import sys
 import re
 from pathlib import Path
-from imageMasking import mask_image_with_bboxes
-from userProcess import LabelingToolByNewImage, LabelingToolBySampleImage
-
+import app
 current_path = Path(__file__).resolve().parent
 sys.path.append(str(current_path / 'UI'))
+
+from app import mask_image_with_bboxes
+from app import LabelingToolByNewImage, LabelingToolBySampleImage
+
+##from app.gui.UI.ui_1 import ui_1, ui_2, ui_3, ui_4, ui_5, ui_6, ui_7, ui_8, ui_help
+from app.gui.UI.ui_1 import Ui_MainWindow1
+from app.gui.UI.ui_2 import Ui_MainWindow2
+from app.gui.UI.ui_3 import Ui_MainWindow3
+from app.gui.UI.ui_4 import Ui_MainWindow4
+from app.gui.UI.ui_5 import Ui_MainWindow5
+from app.gui.UI.ui_6 import Ui_MainWindow6
+from app.gui.UI.ui_7 import Ui_MainWindow7
+from app.gui.UI.ui_8 import Ui_MainWindow8
+from app.gui.UI.ui_help import Ui_MainWindowhelp
 
 from enum import Enum
 import os
@@ -16,8 +28,9 @@ import shutil
 from PySide6.QtWidgets import QApplication, QMainWindow, QListView, QPushButton, QFileDialog, QMessageBox, QLabel, QVBoxLayout, QWidget, QListWidget
 from PySide6.QtCore import QDir, Qt, Slot, QModelIndex
 from PySide6.QtGui import QStandardItemModel, QStandardItem, QPixmap
-import UI
-script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+abc = os.path.dirname(os.path.abspath(app.__file__))
+script_directory = os.path.join(abc, '..')
 
 class CheckableItem(QStandardItem):
     def __init__(self, text, is_folder=False):
@@ -29,13 +42,13 @@ class CheckableItem(QStandardItem):
 class UI_1App(QMainWindow):
     def __init__(self):
         super(UI_1App, self).__init__()
-        self.ui = UI.Ui_MainWindow1()
+        self.ui = Ui_MainWindow1()
         self.ui.setupUi(self)
         self.ui.pushButton_3.clicked.connect(self.close_ui_1_and_open_ui_2)
         self.ui.pushButton_8.clicked.connect(self.close_ui_1_and_open_ui_3)
         self.ui.pushButton_6.clicked.connect(self.close_ui_1_and_open_ui_4)
         self.ui.pushButton.clicked.connect(self.close_ui_1_and_open_ui_6)
-        self.show_file_dictionary(os.path.join(os.getcwd(),'app', 'gui', 'Results'))
+        self.show_file_dictionary(os.path.join(script_directory,'app', 'gui', 'Results'))
         self.ui.pushButton_4.clicked.connect(self.download_files)
         self.ui.listView.doubleClicked.connect(self.handle_listview_doubleclick)
         self.checked_item = ""
@@ -64,7 +77,7 @@ class UI_1App(QMainWindow):
         item = self.ui.listView.model().itemFromIndex(index)
         if item is not None:
             file_name = item.text()
-            file_path = os.path.join(os.getcwd(), 'app', 'gui', 'Results', self.folder_item, file_name)
+            file_path = os.path.join(script_directory, 'app', 'gui', 'Results', self.folder_item, file_name)
             self.selected_file_path = file_path
             
             pixmap = QPixmap(file_path)
@@ -94,7 +107,7 @@ class UI_1App(QMainWindow):
             selected_items = [model.item(i).text() for i in range(model.rowCount()) if model.item(i).checkState() == Qt.Checked]
 
             for item_text in selected_items:
-                source_path = os.path.join(os.getcwd(), 'app/gui/Results', self.folder_item, item_text)
+                source_path = os.path.join(script_directory, 'app/gui/Results', self.folder_item, item_text)
                 destination_path = os.path.join(download_folder, item_text)
 
                 try:
@@ -146,9 +159,9 @@ class UI_1App(QMainWindow):
     @Slot("QModelIndex")
     def handle_listview_doubleclick(self, index):
         item = self.ui.listView.model().itemFromIndex(index)
-        if item and os.path.isdir(os.path.join(os.getcwd(), 'app/gui/Results', item.text())):
+        if item and os.path.isdir(os.path.join(script_directory, 'app/gui/Results', item.text())):
             self.folder_item = item.text()
-            self.show_file_list(os.path.join(os.getcwd(), 'app/gui/Results', item.text()))
+            self.show_file_list(os.path.join(script_directory, 'app/gui/Results', item.text()))
             
 
     @Slot()
@@ -178,7 +191,7 @@ class UI_1App(QMainWindow):
 class UI_2App(QMainWindow):
     def __init__(self):
         super(UI_2App, self).__init__()
-        self.ui = UI.Ui_MainWindow2()
+        self.ui = Ui_MainWindow2()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.close_ui_2_and_open_ui_help)
         self.ui.pushButton_7.clicked.connect(self.close_ui_2_and_open_ui_1)
@@ -219,7 +232,7 @@ class UI_2App(QMainWindow):
 class UI_3App(QMainWindow):
     def __init__(self):
         super(UI_3App, self).__init__()
-        self.ui = UI.Ui_MainWindow3()
+        self.ui = Ui_MainWindow3()
         self.ui.setupUi(self)
         self.model = QStandardItemModel()
         self.ui.pushButton_7.clicked.connect(self.close_ui_3_and_open_ui_1)
@@ -228,7 +241,9 @@ class UI_3App(QMainWindow):
         self.ui.pushButton_6.clicked.connect(self.close_ui_3_and_open_ui_6)
         self.ui.pushButton_4.clicked.connect(self.run_predict_process)
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        directory = os.path.join(os.getcwd(), 'app', 'gui' 'down')
+        directory = os.path.join(script_directory, 'app', 'gui', 'down')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
         self.show_file_list(directory)
 
     def show_file_list(self, directory):
@@ -249,7 +264,7 @@ class UI_3App(QMainWindow):
     def upload_file(self):
         file_dialog = QFileDialog()
         file_paths, _ = file_dialog.getOpenFileNames(self, "파일 선택", "", "모든 파일 (*);;텍스트 파일 (*.txt);;이미지 파일 (*.png *.jpg)")
-        destination_directory = os.path.join(self.current_dir, 'down')
+        destination_directory = os.path.join(script_directory, 'app', 'gui', 'down')
 
         for file_path in file_paths:
             destination_path = os.path.join(destination_directory, os.path.basename(file_path))
@@ -277,7 +292,7 @@ class UI_3App(QMainWindow):
 
         if selected_items:
             file_name = selected_items[0].text()
-            directory = os.path.join(os.getcwd(), 'app','gui', 'down')
+            directory = os.path.join(script_directory, 'app','gui', 'down')
             file_path = os.path.join(directory, file_name)
             return file_path
         else:
@@ -302,7 +317,7 @@ class UI_3App(QMainWindow):
         selected_file_name = self.get_selected_file_name()
 
         if selected_file_name:
-            other_directory = os.path.join(self.current_dir, '..', 'Model')
+            other_directory = os.path.join(script_directory, 'app', 'Model')
             predict_process_path = os.path.join(other_directory, 'predictProcess.py')
             function_name = 'ser_re'
 
@@ -311,24 +326,25 @@ class UI_3App(QMainWindow):
                 file_name, _ = os.path.splitext(base_name)
 
                 for dir_name in ['SampleRepo', 'Results']:
-                    dir_path = os.path.join(os.getcwd(), 'app', 'gui', dir_name)
+                    dir_path = os.path.join(script_directory, 'app', 'gui', dir_name)
                     specific_dir_path = os.path.join(dir_path, file_name)
 
                     for path in [dir_path, specific_dir_path]:
                         if not os.path.exists(path):
                             os.makedirs(path)
 
-                sample_repo_dir = os.path.join(os.getcwd(), 'app', 'gui', 'SampleRepo', file_name)
+                sample_repo_dir = os.path.join(script_directory, 'app', 'gui', 'SampleRepo', file_name)
                 shutil.copy2(selected_file_name, sample_repo_dir)
-
-                subprocess.run(["python", predict_process_path, function_name, selected_file_name])
+                print("씨발")
+                subprocess.run(["python3", predict_process_path, function_name, selected_file_name])
+                print("씨발")
                 QMessageBox.information(self,"알림","Privacy Detection이 완료되었습니다.\nSample List에서 Masking을 진행하세요.")
 
             except Exception as e:
                 print(f"An error occurred: {e}")
 
         file_name = os.path.basename(specific_dir_path)
-        file_path = os.path.join(self.current_dir, 'SampleRepo', file_name)
+        file_path = os.path.join(script_directory, 'app', 'gui', 'SampleRepo', file_name)
         bbox_path = os.path.join(file_path, file_name + '_privacy_bbox.txt')
         image_path = self.get_selected_file_name()
         image_directory_path = os.path.join(file_path, os.path.basename(selected_file_name))
@@ -367,14 +383,14 @@ class UI_3App(QMainWindow):
 class UI_4App(QMainWindow):
     def __init__(self):
         super(UI_4App, self).__init__()
-        self.ui = UI.Ui_MainWindow4()
+        self.ui = Ui_MainWindow4()
         self.ui.setupUi(self)
         self.ui.pushButton_7.clicked.connect(self.close_ui_4_and_open_ui_1)
         self.ui.pushButton_3.clicked.connect(self.close_ui_4_and_open_ui_2)
         self.ui.pushButton_8.clicked.connect(self.close_ui_4_and_open_ui_3)
         self.ui.pushButton_6.clicked.connect(self.close_ui_4_and_open_ui_6)
 
-        self.show_file_dictionary(os.path.join(os.getcwd(), 'app/gui/Results'))
+        self.show_file_dictionary(os.path.join(script_directory, 'app/gui/Results'))
         self.ui.listView.doubleClicked.connect(self.handle_listview_doubleclick)
         self.ui.pushButton_5.clicked.connect(self.run_labeling_tool)
         self.checked_item = ""
@@ -397,7 +413,7 @@ class UI_4App(QMainWindow):
         item = self.ui.listView.model().itemFromIndex(index)
         if item is not None:
             file_name = item.text()
-            file_path = os.path.join(os.getcwd(), 'app', 'gui', 'Results', self.folder_item, file_name)
+            file_path = os.path.join(script_directory, 'app', 'gui', 'Results', self.folder_item, file_name)
             self.selected_file_path = file_path
 
             pixmap = QPixmap(file_path)
@@ -425,9 +441,9 @@ class UI_4App(QMainWindow):
         basename, ext = os.path.splitext(image_name)
         print(basename)
         
-        image_path = os.path.join(os.getcwd(), 'app', 'gui', 'Results',  aa)
-        image_directory_path = os.path.join(os.getcwd(), 'app', 'gui', 'SampleRepo', aa, 'Upload', image_name)
-        bbox_path = os.path.join(os.getcwd(), 'app', 'gui', 'Results', aa, basename +'.txt')
+        image_path = os.path.join(script_directory, 'app', 'gui', 'Results',  aa)
+        image_directory_path = os.path.join(script_directory, 'app', 'gui', 'SampleRepo', aa, 'Upload', image_name)
+        bbox_path = os.path.join(script_directory, 'app', 'gui', 'Results', aa, basename +'.txt')
         print(image_path, image_directory_path, bbox_path)
 
         self.labeling_tool_window = LabelingToolBySampleImage(image_path, image_directory_path , bbox_path)
@@ -465,9 +481,9 @@ class UI_4App(QMainWindow):
     @Slot("QModelIndex")
     def handle_listview_doubleclick(self, index):
         item = self.ui.listView.model().itemFromIndex(index)
-        if item and os.path.isdir(os.path.join(os.getcwd(), 'app/gui/Results', item.text())):
+        if item and os.path.isdir(os.path.join(script_directory, 'app/gui/Results', item.text())):
             self.folder_item = item.text()
-            self.show_file_list(os.path.join(os.getcwd(), 'app/gui/Results', item.text()))
+            self.show_file_list(os.path.join(script_directory, 'app/gui/Results', item.text()))
 
     @Slot()
     def close_ui_4_and_open_ui_1(self):
@@ -496,7 +512,7 @@ class UI_4App(QMainWindow):
 class UI_6App(QMainWindow):
     def __init__(self):
         super(UI_6App, self).__init__()
-        self.ui = UI.Ui_MainWindow6()
+        self.ui = Ui_MainWindow6()
         self.ui.setupUi(self)
         self.ui.pushButton_7.clicked.connect(self.close_ui_6_and_open_ui_1)
         self.ui.pushButton_3.clicked.connect(self.close_ui_6_and_open_ui_2)
@@ -505,7 +521,7 @@ class UI_6App(QMainWindow):
         self.ui.pushButton_4.clicked.connect(self.run_labeling_tool)
 
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        directory = os.path.join(self.current_dir, 'SampleRepo')
+        directory = os.path.join(script_directory, 'app', 'gui', 'SampleRepo')
         self.show_directory_list(directory)
 
         self.selected_file_path = ""
@@ -568,7 +584,7 @@ class UI_6App(QMainWindow):
 
         if selected_items:
             file_name = selected_items[0].text()
-            directory = os.path.join('app','gui', 'down')
+            directory = os.path.join(script_directory, 'app','gui', 'down')
             file_path = os.path.join(directory, file_name)
             return file_path
         else:
@@ -579,12 +595,12 @@ class UI_6App(QMainWindow):
         file_base_path, _ = os.path.splitext(self.selected_file_path)
         file_name = os.path.basename(file_base_path)
         image_path = self.get_selected_file_name()
-        bbox_path = os.path.join(os.getcwd(), 'app', 'gui', 'SampleRepo', file_name, file_name + '_privacy_bbox.txt')
+        bbox_path = os.path.join(script_directory, 'app', 'gui', 'SampleRepo', file_name, file_name + '_privacy_bbox.txt')
         self.labeling_tool_window = LabelingToolBySampleImage(image_path, self.selected_file_path, bbox_path)
         self.labeling_tool_window.show()
-        print("d" + image_path)
         print(self.selected_file_path)
         print(bbox_path)
+        print(image_path)
 
     @Slot()
     def close_ui_6_and_open_ui_1(self):
@@ -615,7 +631,7 @@ class UI_6App(QMainWindow):
         item = self.ui.listView.model().itemFromIndex(index)
         if item is not None:
             file_name = item.text()
-            file_path = os.path.join(self.current_dir, 'down', file_name)
+            file_path = os.path.join(script_directory, 'app', 'gui', 'down', file_name)
             self.selected_file_path = file_path
             
             pixmap = QPixmap(file_path)
@@ -646,7 +662,7 @@ class UI_6App(QMainWindow):
 class UI_8App(QMainWindow):
     def __init__(self, directory):
         super(UI_8App, self).__init__()
-        self.ui = UI.Ui_MainWindow8()
+        self.ui = Ui_MainWindow8()
         self.ui.setupUi(self)
         self.model = QStandardItemModel()
         self.current_dir = directory
@@ -711,15 +727,15 @@ class UI_8App(QMainWindow):
                 base_name = os.path.basename(selected_file_name)
                 file_name, _ = os.path.splitext(base_name)
                 for dir_name in ['SampleRepo', 'Results']:
-                    dir_path = os.path.join(os.getcwd(), 'app', 'gui', dir_name)
+                    dir_path = os.path.join(script_directory, 'app', 'gui', dir_name)
                     specific_dir_path = os.path.join(dir_path, file_name)
                     for path in [dir_path, specific_dir_path]:
                         if not os.path.exists(path):
                             os.makedirs(path)
 
-                sample_repo_dir = os.path.join(os.getcwd(), 'app', 'gui', 'SampleRepo', file_name)
+                sample_repo_dir = os.path.join(script_directory, 'app', 'gui', 'SampleRepo', file_name)
                 shutil.copy2(selected_file_name, sample_repo_dir)
-                subprocess.run(["python", predict_process_path, function_name, selected_file_name])
+                subprocess.run(["python3", predict_process_path, function_name, selected_file_name])
 
             except Exception as e:
                 print(f"An error occurred: {e}")
@@ -730,15 +746,15 @@ class UI_8App(QMainWindow):
         upload_index = parts.index('Upload')  
         Results_folder = parts[upload_index - 1]    
         selected_file_list = self.get_selected_file_paths()
-        save_dir = os.path.join(os.getcwd(), 'app', 'gui', 'Results',Results_folder)
+        save_dir = os.path.join(script_directory, 'app', 'gui', 'Results',Results_folder)
         
         for selected_file in selected_file_list :   
             selected_file_dir = os.path.join(current_dir, selected_file)
             bbox_basename, extension = os.path.splitext(selected_file)
-            bbox_path = os.path.join(os.getcwd(), 'app', 'gui', 'Results', Results_folder, bbox_basename + '.txt')
+            bbox_path = os.path.join(script_directory, 'app', 'gui', 'Results', Results_folder, bbox_basename + '.txt')
             print(selected_file_dir, bbox_path, save_dir)
-            script_path = os.path.join(os.getcwd(), "app", "Model", "SampleProcess.py")
-            subprocess.run(["python", script_path, current_dir, selected_file_dir])
+            script_path = os.path.join(script_directory, "app", "Model", "SampleProcess.py")
+            subprocess.run(["python3", script_path, current_dir, selected_file_dir])
             mask_image_with_bboxes(bbox_path, selected_file_dir, save_dir)
         
         QMessageBox.information(self,"알림","Masking 작업이 완료되었습니다.")
@@ -748,7 +764,7 @@ class UI_8App(QMainWindow):
         #selected_file_name = self.get_selected_file_name()
         #print(selected_file_name)
         #script_path = os.path.join("app", "Model", "predictProcess.py")
-        #subprocess.run(["python", script_path, "ser", self.current_dir])
+        #subprocess.run(["python3", script_path, "ser", self.current_dir])
 
     def delete_selected_files(self, model, directory):
         selected_items = [model.item(i) for i in range(model.rowCount()) if model.item(i).checkState() == Qt.Checked]
@@ -789,7 +805,7 @@ class UI_8App(QMainWindow):
 class UI_helpApp(QMainWindow):
     def __init__(self):
         super(UI_helpApp, self).__init__()
-        self.ui = UI.Ui_MainWindowhelp()
+        self.ui = Ui_MainWindowhelp()
         self.ui.setupUi(self)
         self.ui.pushButton_3.clicked.connect(self.close_ui_help_and_open_ui_2)
     
